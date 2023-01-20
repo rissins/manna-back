37e5bf128e26a9bade0b905216f7526861f8d8c8
rissins <tnalsqqq@naver.com>
2023-01-20 20:40:56 +0900

    initial commit

---
+++ src/main/java/com/rissins/manna/MannaApplication.java
@@ -0,0 +1,13 @@
+package com.rissins.manna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MannaApplication.class, args);
+	}
+
+}
